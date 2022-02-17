0185eda204a2714e00749e4f663dcfac6b615deb
Melanie Couronne <melanie.couronne@gmail.com>
2022-02-17 22:54:38 +0100

    generate user_question model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_214039) do
+ActiveRecord::Schema.define(version: 2022_02_17_215411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,13 @@ ActiveRecord::Schema.define(version: 2022_02_17_214039) do
     t.index ["character_id"], name: "index_user_characters_on_character_id"
   end
 
+  create_table "user_questions", force: :cascade do |t|
+    t.bigint "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_user_questions_on_question_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -142,4 +149,5 @@ ActiveRecord::Schema.define(version: 2022_02_17_214039) do
   add_foreign_key "questions", "characteristics"
   add_foreign_key "rounds", "games"
   add_foreign_key "user_characters", "characters"
+  add_foreign_key "user_questions", "questions"
 end
