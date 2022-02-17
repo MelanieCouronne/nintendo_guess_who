6694ef64c7a496c799755d600266d03a1482b654
Melanie Couronne <melanie.couronne@gmail.com>
2022-02-17 22:11:24 +0100

    generate round model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_210016) do
+ActiveRecord::Schema.define(version: 2022_02_17_211007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,14 @@ ActiveRecord::Schema.define(version: 2022_02_17_210016) do
     t.index ["characteristic_id"], name: "index_questions_on_characteristic_id"
   end
 
+  create_table "rounds", force: :cascade do |t|
+    t.integer "level", default: 0
+    t.bigint "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_rounds_on_game_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -101,4 +109,5 @@ ActiveRecord::Schema.define(version: 2022_02_17_210016) do
   add_foreign_key "character_profiles", "characters"
   add_foreign_key "games", "users"
   add_foreign_key "questions", "characteristics"
+  add_foreign_key "rounds", "games"
 end
