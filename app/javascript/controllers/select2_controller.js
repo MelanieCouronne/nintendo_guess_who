526832056a999bfa2594bee5ac6c3b05306d6355
Melanie Couronne <melanie.couronne@gmail.com>
2022-02-21 20:12:49 +0100

    Fixed bugs to select2

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -5,7 +5,6 @@ import "select2/dist/css/select2.min.css";
 
 export default class extends Controller {
   connect() {
-    console.log("hello")
     $(this.element).select2({
       width: "100%"
     });
