f2a66d894835839b52dc3e9ce0a5eac8e49a169f
Melanie Couronne <melanie.couronne@gmail.com>
2022-03-03 16:20:58 +0100

    Added modal to round

--- app/javascript/plugins/display_btn.js
+++ app/javascript/plugins/display_btn.js
@@ -1,13 +1,13 @@
 const displayBtn = () => {
 
   // Variable
-  const hiddenBtn = document.querySelector('button.d-none');
+  const hiddenBtn = document.querySelector('span.d-none');
   const countBackClass = document.querySelectorAll('.card-circle-back');
   const frontCards = document.querySelectorAll('.card-circle__small');
 
   // Validate form on click
   if (hiddenBtn) {
-    if (countBackClass.length > 21) {
+    if (countBackClass.length >= 21) {
       hiddenBtn.classList.remove('d-none');
       frontCards.forEach((frontCard) => {
         frontCard.classList.add('pulse');
