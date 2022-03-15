f935775c35462eee01b767115f545c44d81f5485
Melanie Couronne <melanie.couronne@gmail.com>
2022-03-15 19:05:58 +0100

    Added checkbox on cards

--- app/javascript/plugins/selected_character.js
+++ app/javascript/plugins/selected_character.js
@@ -1,8 +1,8 @@
 const selectedCharacter = (item) => {
 
   // Variable
-  const triggerBtns = document.querySelectorAll('.card-rounded');
-  console.log(triggerBtns)
+  const triggerBtns = document.querySelectorAll('.position-relative-for-cards');
+
   // Handle border on selected character & validation of form
   if (triggerBtns) {
     triggerBtns.forEach((triggerBtn) => {
@@ -10,18 +10,18 @@ const selectedCharacter = (item) => {
 
         // Variable if an other character is selected
         const borderStickys = document.querySelectorAll('.card-border-red');
-        console.log(event)
+        const checkboxes = document.querySelectorAll('.card-checkbox');
         // Remove border on the other selected character
         if (borderStickys) {
           borderStickys.forEach((borderSticky) => {
             borderSticky.classList.remove('card-border-red');
-            console.log("Hello 1")
+            checkboxes.classList.add('d-none');
           });
         }
 
       // Add border on the selected character
       triggerBtn.classList.add('card-border-red')
-        console.log("hello 2")
+      triggerBtn.classList.remote('d-none')
       });
     });
   }
