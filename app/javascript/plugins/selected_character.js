db71bce3bb6c56d38e080f9705ade827b5b6cdd9
Melanie Couronne <melanie.couronne@gmail.com>
2022-03-16 19:13:36 +0100

    Fixed JS func seletedCharacter for mobile device

--- app/javascript/plugins/selected_character.js
+++ app/javascript/plugins/selected_character.js
@@ -1,7 +1,14 @@
+import { end } from "@popperjs/core";
+
+// const mediaQuery = window.matchMedia('(max-width: 767px)')
+
+
 const selectedCharacter = (item) => {
 
   // Variable
   const allCards = document.querySelectorAll('.box-bounce');
+  // Create a condition that targets viewports at least 767px wide
+  const mediaQuery = window.matchMedia("(max-width: 767px)");
 
   // Handle checkbox on selected character
   if (allCards) {
@@ -10,22 +17,27 @@ const selectedCharacter = (item) => {
 
         // Variable if an other character is already selected
         const allCheckboxes = document.querySelectorAll('.card-checkbox');
-        const cardToCheck = card.querySelector('.card-checkbox')
-        console.log(cardToCheck)
+        const cardToCheck = card.querySelector('.card-checkbox');
 
         // Remove all other checkboxes on other characters
         if (allCheckboxes) {
           allCheckboxes.forEach((checkbox) => {
             checkbox.classList.add('d-none');
+            if (mediaQuery.matches === true) {
+              checkbox.classList.remove('pulse-green');
+            } else {
+              checkbox.classList.remove('pulse-red');
+            };
           });
         }
 
       // Add checkbox on new selected character
-      cardToCheck.classList.remove('d-none')
+      cardToCheck.classList.remove('d-none');
+      cardToCheck.classList.add('pulse-green');
+
       });
     });
   }
-
 }
 
 export { selectedCharacter };
