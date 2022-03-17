12a34078d8dadb4e5422581d69b2e3c76c79f233
Melanie Couronne <melanie.couronne@gmail.com>
2022-03-17 18:54:43 +0100

    Added a theme to Select2

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -6,12 +6,13 @@ import "select2/dist/css/select2.min.css";
 export default class extends Controller {
   connect() {
     $(this.element).select2({
-      width: "100%"
+      width: "330px"
     });
 
     $(".js-example-placeholder-single").select2({
       placeholder: "Please, select a question",
-      allowClear: true
+      allowClear: true,
+      theme: "classic"
     });
 
     // $(".js-example-basic-multiple-limit").select2({
