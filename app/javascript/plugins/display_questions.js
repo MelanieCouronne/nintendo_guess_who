604d3329d3963a63ddd30f98c9cfd45b6ee07d15
Melanie Couronne <melanie.couronne@gmail.com>
2022-04-04 15:26:51 +0200

    Fix white card for Questions to JS

--- app/javascript/plugins/display_questions.js
+++ app/javascript/plugins/display_questions.js
@@ -1,7 +1,7 @@
 const displayQuestions = () => {
 
   // Variable for Questions
-  const mediaQuery = window.matchMedia("(max-width: 767px)");
+  const mediaQuery = window.matchMedia("(max-width: 991px)");
   const formForSmDevice = document.querySelector('#question-for-sm-device');
   const formForMdDevice = document.querySelector('#question-for-md-device');
 
@@ -10,24 +10,35 @@ const displayQuestions = () => {
   const countBackClass = document.querySelectorAll('.card-rounded-back');
   const frontCards = document.querySelectorAll('.card-rounded-round');
 
-  // Validate form on click
-  if (formForSmDevice) {
-    if (mediaQuery.matches === true) {
-      // Display button
-      if (countBackClass.length >= 22) {
-        guesswhoBtn.classList.remove('d-none');
-        formForSmDevice.classList.add('d-none');
-        formForMdDevice.classList.add('d-none');
 
-        // frontCards.forEach((frontCard) => {
-        //   frontCard.classList.add('pulse-white-large');
-        // });
-      }
-    } else {
+
+  if (formForSmDevice) {
+    if (countBackClass.length >= 22) {
+      guesswhoBtn.classList.remove('d-none');
+      formForSmDevice.classList.add('d-none');
+      formForMdDevice.classList.add('d-none');
+    } else if (mediaQuery.matches !== true) {
       formForMdDevice.classList.remove('d-none');
       formForSmDevice.classList.add('d-none');
     }
   }
+
+  // // Validate form on click
+  // if (formForSmDevice) {
+  //   if (mediaQuery.matches === true) {
+  //     // Display button Guess who
+  //     if (countBackClass.length >= 22) {
+  //       guesswhoBtn.classList.remove('d-none');
+  //       formForSmDevice.classList.add('d-none');
+  //       formForMdDevice.classList.add('d-none');
+  //     } else { // Display Question card above characters cards
+  //       formForSmDevice.classList.remove('d-none');
+  //     };
+  //   } else {
+  //     formForMdDevice.classList.remove('d-none');
+  //     formForSmDevice.classList.add('d-none');
+  //   }
+  // }
 }
 
 export { displayQuestions };
