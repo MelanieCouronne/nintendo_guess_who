920fe3c2b359efb6ca80f6291444418719edd8ac
Melanie Couronne <melanie.couronne@gmail.com>
2022-04-13 12:29:51 +0200

    Added profile to routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  # devise_for :users, controllers: { registrations: 'users/registrations'}
+  get 'users/profile'
   root to: 'games#new' #'pages#home'
 
   resources :games, only: [:new, :create, :show] do
