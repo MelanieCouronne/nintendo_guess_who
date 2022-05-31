2ad91f29b4df9d3d62010d1181064d155d219741
Melanie Couronne <melanie.couronne@gmail.com>
2022-05-31 19:48:20 +0200

    fixed devise for photo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
   def profile
-    @user = current_user
   end
 
   def edit
-    @user = current_user
   end
 
   def update
-    @user = current_user
-    @user.update!
+    @user.update(user_params)
+
     redirect_to user_profile_path(@user)
   end
 
   private
 
-  def article_params
-    params.require(:user).permit(:email, :password, :photo)
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :photo)
   end
 end
