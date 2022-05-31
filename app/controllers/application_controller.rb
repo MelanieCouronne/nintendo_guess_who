7048863c5a4e4a1575b5148521cca90366bab6ff
Melanie Couronne <melanie.couronne@gmail.com>
2022-05-31 20:23:08 +0200

    Changed devise config in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email])
   end
 end
