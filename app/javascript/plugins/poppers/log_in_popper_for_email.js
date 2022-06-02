9da2689ccd3d73f3ac9cb766a3c3a13d8124672e
Melanie Couronne <melanie.couronne@gmail.com>
2022-06-02 16:26:12 +0200

    Added popper to edit link

--- app/javascript/plugins/poppers/log_in_popper_for_email.js
+++ app/javascript/plugins/poppers/log_in_popper_for_email.js
@@ -1,79 +1,80 @@
 import { createPopper } from '@popperjs/core';
-
 // For Email
 const arrowEmail = document.querySelector('#arrowEmail');
 const tooltipEmail = document.querySelector('#tooltipEmail');
 // targets to display popper tooltip and trigger event
 const cloneIconForEmail = document.querySelector('#cloneIconForEmail');
 
+if (cloneIconForEmail) {
 
-// Create popper for Email clone
-const popperInstanceEmail = createPopper(cloneIconForEmail, tooltipEmail, {
-  placement: 'left-start',
-  modifiers: [
-    {
-      name: 'arrow',
-      options: {
-        element: arrowEmail,
-      },
-    },
-    {
-      name: 'offset',
-      options: {
-        offset: [0, 5],
-      },
-    },
-    {
-      name: 'flip',
-      options: {
-        fallbackPlacements: ['top', 'right'],
-      },
-    },
-  ],
-});
-
-
-// Display popper for Email
-function show() {
-  // Make the tooltip visible
-  tooltipEmail.setAttribute('data-show', '');
-
-  // Enable the event listeners
-  popperInstanceEmail.setOptions((options) => ({
-    ...options,
+  // Create popper for Email clone
+  const popperInstanceEmail = createPopper(cloneIconForEmail, tooltipEmail, {
+    placement: 'left-start',
     modifiers: [
-      ...options.modifiers,
-      { name: 'eventListeners', enabled: true },
+      {
+        name: 'arrow',
+        options: {
+          element: arrowEmail,
+        },
+      },
+      {
+        name: 'offset',
+        options: {
+          offset: [0, 5],
+        },
+      },
+      {
+        name: 'flip',
+        options: {
+          fallbackPlacements: ['top', 'right'],
+        },
+      },
     ],
-  }));
+  });
 
-  // Update its position
-  popperInstanceEmail.update();
+
+  // Display popper for Email
+  function show() {
+    // Make the tooltip visible
+    tooltipEmail.setAttribute('data-show', '');
+
+    // Enable the event listeners
+    popperInstanceEmail.setOptions((options) => ({
+      ...options,
+      modifiers: [
+        ...options.modifiers,
+        { name: 'eventListeners', enabled: true },
+      ],
+    }));
+
+    // Update its position
+    popperInstanceEmail.update();
+  }
+
+  function hide() {
+    // Hide the tooltip
+    tooltipEmail.removeAttribute('data-show');
+
+    // Disable the event listeners
+    popperInstanceEmail.setOptions((options) => ({
+      ...options,
+      modifiers: [
+        ...options.modifiers,
+        { name: 'eventListeners', enabled: false },
+      ],
+    }));
+  }
+
+
+  // triggers and Events for Email
+  const showEvents = ['click'];
+  const hideEvents = ['mouseleave', 'blur'];
+
+  showEvents.forEach((event) => {
+    cloneIconForEmail.addEventListener(event, show);
+  });
+
+  hideEvents.forEach((event) => {
+    cloneIconForEmail.addEventListener(event, hide);
+  });
 }
-
-function hide() {
-  // Hide the tooltip
-  tooltipEmail.removeAttribute('data-show');
-
-  // Disable the event listeners
-  popperInstanceEmail.setOptions((options) => ({
-    ...options,
-    modifiers: [
-      ...options.modifiers,
-      { name: 'eventListeners', enabled: false },
-    ],
-  }));
-}
-
-
-// triggers and Events for Email
-const showEvents = ['click'];
-const hideEvents = ['mouseleave', 'blur'];
-
-showEvents.forEach((event) => {
-  cloneIconForEmail.addEventListener(event, show);
-});
-
-hideEvents.forEach((event) => {
-  cloneIconForEmail.addEventListener(event, hide);
-});
--- app/javascript/plugins/poppers/log_in_popper_for_password.js
+++ app/javascript/plugins/poppers/log_in_popper_for_password.js
@@ -6,73 +6,75 @@ const tooltipPassword = document.querySelector('#tooltipPassword');
 // targets to display popper tooltip and trigger event
 const cloneIconForPassword = document.querySelector('#cloneIconForPassword');
 
-// Create popper for Password clone
-const popperInstancePassword = createPopper(cloneIconForPassword, tooltipPassword, {
-  placement: 'left-start',
-  modifiers: [
-    {
-      name: 'arrow',
-      options: {
-        element: arrowPassword,
-      },
-    },
-    {
-      name: 'offset',
-      options: {
-        offset: [0, 5],
-      },
-    },
-    {
-      name: 'flip',
-      options: {
-        fallbackPlacements: ['top', 'right'],
-      },
-    },
-  ],
-});
-
-
-// Display popper for Password
-function show() {
-  // Make the tooltip visible
-  tooltipPassword.setAttribute('data-show', '');
-
-  // Enable the event listeners
-  popperInstancePassword.setOptions((options) => ({
-    ...options,
+if (cloneIconForPassword) {
+  // Create popper for Password clone
+  const popperInstancePassword = createPopper(cloneIconForPassword, tooltipPassword, {
+    placement: 'left-start',
     modifiers: [
-      ...options.modifiers,
-      { name: 'eventListeners', enabled: true },
+      {
+        name: 'arrow',
+        options: {
+          element: arrowPassword,
+        },
+      },
+      {
+        name: 'offset',
+        options: {
+          offset: [0, 5],
+        },
+      },
+      {
+        name: 'flip',
+        options: {
+          fallbackPlacements: ['top', 'right'],
+        },
+      },
     ],
-  }));
+  });
 
-  // Update its position
-  popperInstancePassword.update();
+
+  // Display popper for Password
+  function show() {
+    // Make the tooltip visible
+    tooltipPassword.setAttribute('data-show', '');
+
+    // Enable the event listeners
+    popperInstancePassword.setOptions((options) => ({
+      ...options,
+      modifiers: [
+        ...options.modifiers,
+        { name: 'eventListeners', enabled: true },
+      ],
+    }));
+
+    // Update its position
+    popperInstancePassword.update();
+  }
+
+  function hide() {
+    // Hide the tooltip
+    tooltipPassword.removeAttribute('data-show');
+
+    // Disable the event listeners
+    popperInstancePassword.setOptions((options) => ({
+      ...options,
+      modifiers: [
+        ...options.modifiers,
+        { name: 'eventListeners', enabled: false },
+      ],
+    }));
+  }
+
+
+  // triggers and Events for Password
+  const showEvents = ['click'];
+  const hideEvents = ['mouseleave', 'blur'];
+
+  showEvents.forEach((event) => {
+    cloneIconForPassword.addEventListener(event, show);
+  });
+
+  hideEvents.forEach((event) => {
+    cloneIconForPassword.addEventListener(event, hide);
+  });
 }
-
-function hide() {
-  // Hide the tooltip
-  tooltipPassword.removeAttribute('data-show');
-
-  // Disable the event listeners
-  popperInstancePassword.setOptions((options) => ({
-    ...options,
-    modifiers: [
-      ...options.modifiers,
-      { name: 'eventListeners', enabled: false },
-    ],
-  }));
-}
-
-
-// triggers and Events for Password
-const showEvents = ['click'];
-const hideEvents = ['mouseleave', 'blur'];
-
-showEvents.forEach((event) => {
-  cloneIconForPassword.addEventListener(event, show);
-});
-
-hideEvents.forEach((event) => {
-  cloneIconForPassword.addEventListener(event, hide);
-});
