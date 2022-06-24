a4f89d8d45190c124190169f350a35994642fbb9
Melanie Couronne <melanie.couronne@gmail.com>
2022-06-24 17:55:16 +0200

    Refacto game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,25 +16,24 @@ class GamesController < ApplicationController
     @game.user = @user
 
     # Handle the user character's choice
-    @user_character_id = params[:game][:id].to_i
-    @user_character = UserCharacter.create!(character_id: @user_character_id)
+    user_character_choice = params[:game][:id].to_i
+    @user_character = UserCharacter.create!(character_id: user_character_choice)
     @game.user_character = @user_character
     # Handle characteristics to avoid back and forth in the DB
-    @user_character.user_characteristics = UserCharacter.setup_characteristics(@user_character_id)
+    @game.user_character.user_characteristics = UserCharacter.setup_characteristics(user_character_choice)
     @user_character.save
 
     # Handle the computer character's choice
-    @computer_character_id = choose_random_character
-    @computer_character = ComputerCharacter.create!(character_id: @computer_character_id)
+    computer_character_choice = choose_random_character
+    @computer_character = ComputerCharacter.create!(character_id: computer_character_choice)
     @game.computer_character = @computer_character
     # Handle characteristics to avoid back and forth in the DB
-    @computer_character.computer_characteristics = ComputerCharacter.setup_characteristics(@computer_character_id)
+    @game.computer_character.computer_characteristics = ComputerCharacter.setup_characteristics(computer_character_choice)
     @computer_character.save
-
 
     display_characters_to_console # Just to check
 
-    # Save
+    # Save the game and start the first round
     if @game.save # if save => create the first round and redirect
       flash[:notice] = "Your game was successfully created!"
       redirect_to new_game_round_path(@game)
@@ -50,10 +49,7 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
-    puts "################ 1 - User character => name: #{@game.user_character.character.character_name.capitalize} | id: #{@game.user_character.character.id}"
-    puts "################ 2 - Computer character => name: #{@game.computer_character.character.character_name.capitalize} | id: #{@game.computer_character.character.id}"
     @current_status = winner?
-    puts @current_status
     score_gain
     @game.save
   end
@@ -67,14 +63,12 @@ class GamesController < ApplicationController
 
   def display_characters_to_console
     display_title_game
-    user_choice = @game.user_character.character
-    computer_choice = @game.computer_character.character
     puts ""
-    puts "User's choice is => #{user_choice.character_name.capitalize} | id: #{user_choice.id}"
-    puts "#{user_choice.character_name.capitalize}'s characteristics are => #{@user_character.user_characteristics}"
+    puts "User's choice is => #{@game.user_character.character.character_name.capitalize} | character's id: #{@game.user_character.character_id}"
+    puts "#{@game.user_character.character.character_name.capitalize}'s characteristics are => #{@game.user_character.user_characteristics}"
     puts ""
-    puts "Computer's choice is => #{computer_choice.character_name.capitalize} | id: #{computer_choice.id}"
-    puts "#{computer_choice.character_name.capitalize}'s characteristics are => #{@computer_character.computer_characteristics}"
+    puts "Computer's choice is => #{@game.computer_character.character.character_name.capitalize} | character's id: #{@game.computer_character.character_id}"
+    puts "#{@game.computer_character.character.character_name.capitalize}'s characteristics are => #{@game.computer_character.computer_characteristics}"
     puts ""
   end
 
@@ -82,25 +76,21 @@ class GamesController < ApplicationController
     puts <<~HEREDOC
 
 
-                 ,__,   ,  ,__,  -/- _   ,__,   __/   __
-________________/ / (__/__/ / (__/__(/__/ / (__(_/(__(_/(
+                          ,__,   ,  ,__,  -/- _   ,__,   __/   __
+      ___________________/ / (__/__/ / (__/__(/__/ / (__(_/(__(_/(
 
-       __       _   ,    ,              /_   __
-      (_/_(_/__(/__/_)__/_)_____(_/(_/_/ /__(_/(___________________
-      _/_
-     (/
+             __       _   ,    ,              /_   __
+            (_/_(_/__(/__/_)__/_)_____(_/(_/_/ /__(_/(___________________
+            _/_
+          (/
 
     HEREDOC
   end
 
   def winner?
-    if @game.computer_rejected_characters.length == 23
-      false
-    elsif params[:character][:id].to_i == @game.computer_character.character.id
-      true
-    else
-      false
-    end
+    return false if params[:character].blank?
+
+    params[:character][:id].to_i == @game.computer_character.character.id
   end
 
   def score_gain
